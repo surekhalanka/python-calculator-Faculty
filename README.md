7ca195f5d018c8bd5bb4f8517d4d0de0388b1b7c
chinathaip <cartoonabe@gmail.com>
2023-07-25 20:30:51 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Catch the vision - 10th session: Git & GitHub Fundamentals for Software Engineers
+
+
+## Managing Merge Conflicts
---
+++ main.py
@@ -0,0 +1,22 @@
+# Add your function here!
+
+
+
+def main():
+    print("Enter the operation you want to perform: ")
+    user_operation = input()
+    user_input1 = int(input("Enter the first number: "))
+    user_input2 = int(input("Enter the second number: "))
+
+    if user_operation == "add":
+        add(user_input1, user_input2)
+    elif user_operation == "subtract":
+        subtract(user_input1, user_input2)
+    elif user_operation == "multiply":
+        multiply(user_input1, user_input2)
+    elif user_operation == "divide":
+        divide(user_input1, user_input2)
+    elif user_operation == "modulo":
+        modulo(user_input1, user_input2)
+
+main()
